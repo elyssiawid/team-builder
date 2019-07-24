c9188cce644baddea469f1114cc8777044efe202
Elyssia Widjaja <Elyssia@Elyssias-MBP.lan1>
2019-07-24 15:03:05 -0700

    passed props down in app.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,55 +1,71 @@
 import React, { useState } from "react";
-import { Form, TeamMember } from "./components";
-import logo from "./logo.svg";
+// import { Form, TeamMember } from "./components";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const [teamMembers, setTeamMembers] = useState([]);
+  const [updatedMember, setUpdatedMember] = useState({
+    name: "",
+    email: "",
+    role: ""
+  });
   const [memberEdit, setMemberEdit] = useState("");
-}
-// useEffect(() => {
-//   if (localStorage.getItem("teamMembers"))
-//     setTeamMembers(JSON.parse(localStorage.getItem("teamMembers")));
-// }, []);
 
-// useEffect(() => {
-//   localStorage.setItem("teamMembers", JSON.stringify(teamMembers));
-// }, [teamMembers]);
+  // useEffect(() => {
+  //   if (localStorage.getItem("teamMembers"))
+  //     setTeamMembers(JSON.parse(localStorage.getItem("teamMembers")));
+  // }, []);
 
-function addTeamMember(newMember) {
-  setTeamMembers([...teamMembers, { ...newMember, id: teamMembers.length }]);
-}
+  // useEffect(() => {
+  //   localStorage.setItem("teamMembers", JSON.stringify(teamMembers));
+  // }, [teamMembers]);
 
-// function editTeamMember (newMember) {
-//   setTeamMembers(
-//     teamMembers.map(member => {
-//       if (member.id === newMember.id) return newMember;
-//       else return member;
-//     })
-//   );
-//   setMemberToEdit("");
-// };
+  function handleChange(e) {
+    const newMember = {
+      ...updatedMember,
+      [event.target.name]: event.target.value
+    };
+    setTeamMembers(newMember);
+  }
 
-return (
-  <div className="form-group">
-    <h1 className="form">Team Time</h1>
-    <Form
-      addMember={addTeamMember}
-      // editMember={editTeamMember}
-      memberEdit={memberEdit}
-    />
+  // function editTeamMember (newMember) {
+  //   setTeamMembers(
+  //     teamMembers.map(member => {
+  //       if (member.id === newMember.id) return newMember;
+  //       else return member;
+  //     })
+  //   );
+  //   setMemberToEdit("");
+  // };
+
+  return (
     <div className="form-group">
-      {teamMembers.map(member => {
-        return (
-          <TeamMember
-            key={member.id}
-            memberInfo={member}
-            // edit={setMemberEdit}
-          />
-        );
-      })}
+      <h1 className="form">Team</h1>
+      <Form
+        teamMembers={teamMembers}
+        setTeamMembers={setTeamMembers}
+        updatedMember={updatedMember}
+        setUpdateMember={setUpdatedMember}
+        memberEdit={memberEdit}
+        setMemberEdit={setMemberEdit}
+        // addMember={addTeamMember}
+        // // editMember={editTeamMember}
+        // memberEdit={memberEdit}
+      />
+      <div className="form-group">
+        {teamMembers.map(member => {
+          return (
+            <TeamMember
+              key={member.id}
+              memberInfo={member}
+              // edit={setMemberEdit}
+            />
+          );
+        })}
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default App;
